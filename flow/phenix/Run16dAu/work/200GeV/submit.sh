ade389675edd082167654d0c0f929f8c014cd68d
XuQiao <qixu@cern.ch>
2016-10-21 00:00:28 -0500

    Fri Oct 21 00:00:27 CDT 2016

--- flow/phenix/Run16dAu/work/200GeV/submit.sh
+++ flow/phenix/Run16dAu/work/200GeV/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 njobs=106
-for i in $( seq 0 $njobs );do
+#for i in $( seq 0 $njobs );do
+for i in `echo 101 104 105 2 21 24 34 40 42 46 50 6 60 64 65 67 7 82 83 87 88 9`; do
     export I=$i
     if [[ ${1} == "EP" ]]; then
         sbatch -o job$i.out -J 200${1}job$i jobsub.slurm
--- flow/phenix/Run16dAu/work/39GeV/submit.sh
+++ flow/phenix/Run16dAu/work/39GeV/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 njobs=108
-for i in $( seq 0 $njobs );do
+#for i in $( seq 0 $njobs );do
+for i in `echo 16 27 38 47 56 58 64 69 71 73 74 75 76 78 79 88`;do
     export I=$i
     if [[ ${1} == "EP" ]]; then
         sbatch -o job$i.out -J 39${1}job$i jobsub.slurm
