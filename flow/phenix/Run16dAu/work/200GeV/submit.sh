3a8a743cc1378f27f95a3172789ef8d6c44bfe9f
XuQiao <qixu@cern.ch>
2016-10-23 00:00:15 -0500

    Sun Oct 23 00:00:15 CDT 2016

--- flow/phenix/Run16dAu/work/200GeV/submit.sh
+++ flow/phenix/Run16dAu/work/200GeV/submit.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 njobs=106
-for i in $( seq 0 $njobs );do
+#for i in $( seq 0 $njobs );do
+for i in `echo 16`;do
     export I=$i
     if [[ ${1} == "EP" ]]; then
-        sbatch -o job$i.out -J 200${1}job$i jobsub.slurm
+        sbatch --account=cms_stage2 -o job$i.out -J 200${1}job$i jobsub.slurm
     elif [[ ${1} == "Ridge" ]]; then
         sbatch -o job$i.out -J 200${1}job$i jobsubRidge.slurm
     elif [[ ${1} == "Perf" ]]; then
